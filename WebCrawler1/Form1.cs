28500c1be3f8d953d4fa806e29867aaaccb7071a
JiachengSHI <JiachengSHI3616@gmail.com>
2014-08-18 17:16:59 -0500

    initial commit

---
+++ WebCrawler1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebCrawler1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = textBox1.Text;
+            string webPage = Program.getWebPage(url);
+            StreamWriter sw = new StreamWriter("webpage.txt");
+            
+            sw.Write(webPage);
+            sw.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string yfi = "https://finance.yahoo.com/q?s="+textBox2.Text+"&ql=1";
+            Program.getWebData(yfi);
+        }
+
+    }
+}
---
+++ WebCrawler1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using HtmlAgilityPack;
+
+namespace WebCrawler1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        //get WebPage content
+        public static string getWebPage(string url)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            StreamReader sr = new StreamReader(resp.GetResponseStream());
+            string webPage = sr.ReadToEnd();
+            sr.Close();
+            resp.Close();
+            return webPage;
+        }
+
+        //get WebPage data
+        public static void getWebData(string url)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load(url);
+            var name = doc.DocumentNode.SelectSingleNode("//div[@id='yfi_rt_quote_summary']//h2").InnerText;
+            HtmlNode t1 = doc.DocumentNode.SelectSingleNode("//div[@id='yfi_quote_summary_data']//table[@id='table1']");
+            HtmlNode t2 = doc.DocumentNode.SelectSingleNode("//div[@id='yfi_quote_summary_data']//table[@id='table2']");
+            var t1_data = getTable(t1);
+            var t2_data = getTable(t2);
+            var img = doc.DocumentNode.SelectSingleNode("//div[@id='yfi_summary_chart']//img").GetAttributeValue("src", null);
+
+
+            string webData = name + '\n' + t1_data + '\n' + t2_data + '\n' + img;
+            StreamWriter sw = new StreamWriter("webdata.txt");
+            sw.Write(webData);
+            sw.Close();
+        }
+
+        public static string getTable(HtmlNode table)
+        {
+            var data = "";
+            foreach (HtmlNode row in table.SelectNodes("tr"))
+            {
+                data += row.InnerText + ";";
+            }
+            return data;
+        }
+    }
+}
